370e5fd9e01fc7196769044502ab6694e54e66e5
klozz <klozz@thexperienceproject.org>
2021-12-03 09:49:29 +0100

    surya: Track libperfmgr inside device tree
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- device.mk
+++ device.mk
@@ -407,8 +407,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/xiaomi
+    hardware/google/pixel
 
 PRODUCT_BOOT_JARS += \
     tcmiface \
