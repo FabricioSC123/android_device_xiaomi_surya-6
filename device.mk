2a1ba6bd06094129bf078793ccda6efb5cb0877e
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2021-12-03 21:47:08 -0600

    surya: Build servicetracker 1.0 & 1.1
    12-03 21:14:04.174   554 14253 W libc    : Unable to set property "ctl.interface_start" to "vendor.qti.hardware.servicetracker@1.0::IServicetracker/default": error code: 0x20

--- device.mk
+++ device.mk
@@ -427,6 +427,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2-service \
     vendor.qti.hardware.servicetracker@1.2 \
+    vendor.qti.hardware.servicetracker@1.0.vendor \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
     vendor.qti.hardware.servicetracker@1.2.vendor
 
 # Touchscreen
